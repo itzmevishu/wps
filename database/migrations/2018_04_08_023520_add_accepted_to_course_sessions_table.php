7acf2e16d5e23ca0d7316ffb964613feb2fb9b5d
vkalappagari <vkalappagari@innominds.com>
2018-04-08 20:15:16 +0530

    fixing css

---
+++ database/migrations/2018_04_08_023520_add_accepted_to_course_sessions_table.php
@@ -0,0 +1,31 @@
+<?php
+
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class AddAcceptedToCourseSessionsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::table('course_sessions', function (Blueprint $table) {
+            $table->integer('accepted')->default(0)->after('slots');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::table('course_sessions', function (Blueprint $table) {
+            //
+        });
+    }
+}
